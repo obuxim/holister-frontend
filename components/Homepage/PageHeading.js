dce29427a296c9d3361f7e7c6eaf0006eaee5aae
Zubo <z@duoneos.com>
2021-03-19 20:59:19 +0600

    Vegitables added

--- components/Homepage/PageHeading.js
+++ components/Homepage/PageHeading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PageHeading = ({ title }) => {
-    return <p className='fs-1 fw-700 text-center mt-4 mt-md-5'>{title}</p>;
+  return <p className='text-uppercase fs-1 fw-700 text-center mt-4 mt-md-5'>{title}</p>;
 };
 
 export default PageHeading;
--- pages/shop.js
+++ pages/shop.js
@@ -1,46 +1,77 @@
-import React from "react";
-import PageHeading from "../components/Homepage/PageHeading";
-import ProductCard from "../components/ProductCard";
+import React from 'react';
+import PageHeading from '../components/Homepage/PageHeading';
+import ProductCard from '../components/ProductCard';
 
-const products = [
-  {
-    id: 1,
-    title: "Shrimp Golda (10/15 pcs) 500 GM",
-  },
-  {
-    id: 2,
-    title: "Deshi Sarputi Fish (3/4 pcs) 500 GM",
-  },
-  {
-    id: 3,
-    title: "Whole Halisha (500 GM)",
-  },
-  {
-    id: 4,
-    title: "Poa Fish (500 GM)",
-  },
-  {
-    id: 5,
-    title: "Black Crap After Cutting (1.5 KG)",
-  },
-  {
-    id: 6,
-    title: "Whole Halisha (900 GM)",
-  },
-];
+const products = {
+  fish: [
+    {
+      id: 1,
+      title: 'Shrimp Golda (10/15 pcs) 500 GM',
+    },
+    {
+      id: 2,
+      title: 'Deshi Sarputi Fish (3/4 pcs) 500 GM',
+    },
+    {
+      id: 3,
+      title: 'Whole Halisha (500 GM)',
+    },
+    {
+      id: 4,
+      title: 'Poa Fish (500 GM)',
+    },
+    {
+      id: 5,
+      title: 'Black Crap After Cutting (1.5 KG)',
+    },
+    {
+      id: 6,
+      title: 'Whole Halisha (900 GM)',
+    },
+  ],
+  vegitables: [
+    {
+      id: 7,
+      title: 'Onion (Red)',
+    },
+    {
+      id: 8,
+      title: 'Potato',
+    },
+    {
+      id: 9,
+      title: 'Red Tomato',
+    },
+    {
+      id: 10,
+      title: 'Cauliflower',
+    },
+    {
+      id: 11,
+      title: 'Corilander Leaves',
+    },
+    {
+      id: 12,
+      title: 'Garlic',
+    },
+  ],
+};
 
 const shop = () => {
   return (
     <>
-      <PageHeading title="Cheapest price in country guranteed" />
-      <div className="container">
-        <div className="row">
-          {products.map((product) => (
-            <ProductCard
-              id={product.id}
-              title={product.title}
-              key={product.id}
-            />
+      <PageHeading title='Cheapest price in country guranteed' />
+      <div className='container'>
+        <div className='row mt-4'>
+          <h3>Fish</h3>
+          {products.fish.map((product) => (
+            <ProductCard id={product.id} title={product.title} key={product.id} />
+          ))}
+        </div>
+        <div className='row mt-4'>
+          <h3>Vegitables</h3>
+          {products.vegitables.map((product) => (
+            <ProductCard id={product.id} title={product.title} key={product.id} />
           ))}
         </div>
       </div>
