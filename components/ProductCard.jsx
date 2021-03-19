d71c5e988028c94d4f33367346334ed1c1f5688b
Zubair Hasan Zubo <zubairhasan@staffasia.org>
2021-03-19 15:09:18 +0600

    ShopCard made responsive

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -6,7 +6,7 @@ const ProductCard = ({ id, title }) => {
     setImage(`/products/products-${id}.png`);
   }, []);
   return (
-    <div className="col-4 p-2">
+    <div className="col-6 col-md-4 p-2">
       <div className="card shadow">
         <img src={image} alt="" className="card-top-img" />
         <div className="card-body p-3 text-center">
